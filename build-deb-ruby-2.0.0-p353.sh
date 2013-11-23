03b7055bf888748d32f7b0f02d77feef04397676
darron froese <darron@froese.org>
2013-11-22 21:19:37 -0700

    Adding some deps and removing sudo for building inside a Docker container.

--- build-deb-ruby-2.0.0-p353.sh
+++ build-deb-ruby-2.0.0-p353.sh
@@ -7,7 +7,7 @@ rubysrc=ruby-$rubyversion.tar.bz2
 checksum=20eb8f067d20f6b76b7e16cce2a85a55
 destdir=/tmp/install-$rubyversion
 
-sudo apt-get -y install libssl-dev libreadline-dev zlib1g-dev libyaml-dev libgdbm-dev libffi-dev libncurses5-dev libxml2-dev libxslt1-dev automake libtool libc6-dev
+apt-get -y install bzip2 wget libssl-dev libreadline-dev zlib1g-dev libyaml-dev libgdbm-dev libffi-dev libncurses5-dev libxml2-dev libxslt1-dev automake libtool libc6-dev
 
 if [ ! -f $rubysrc ]; then
     wget -q ftp://ftp.ruby-lang.org/pub/ruby/2.0/$rubysrc
@@ -24,7 +24,7 @@ cd ruby-$rubyversion
 ./configure --prefix=/usr/local --disable-install-doc --enable-shared && make && make install DESTDIR=$destdir
 
 cd ..
-gem list -i fpm || sudo gem install fpm
+gem list -i fpm || gem install fpm
 fpm -s dir -t deb -n ruby$version -v $rubyversion -C $destdir \
   -p ruby-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
   -d "libc6 (>= 2.6)" -d "libffi6 (>= 3.0.10)" -d "libgdbm3 (>= 1.8.3)" \
